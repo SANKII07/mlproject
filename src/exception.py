1c05890df1e0dc50b97fbda75a0969baac3af64c
Sankalp <sankalpupadhyay2702@gmail.com>
2025-09-03 20:10:44 +0530

    Jupyter notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -15,11 +15,5 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)
+
 
